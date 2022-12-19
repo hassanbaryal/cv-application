a442a52b6ec6a494793aa2d3885ea6b7026ea4d4
hassanbaryal <h.baryal2@gmail.com>
2022-12-18 19:25:48 -0500

    Styling changes

--- src/components/GeneralInformation.js
+++ src/components/GeneralInformation.js
@@ -24,9 +24,9 @@ export default class GeneralInformation extends React.Component {
 
   render() {
     return(
-      <div className='flex flex-col text-emerald-500 gap-1'>
+      <div className='text-emerald-500 flex flex-col gap-1'>
         <input className='text-5xl p-2  border-b-4 border-emerald-500' id='name' type='text' value={this.state.name} onChange={this.handleChange} />
-        <div className='flex justify-start gap-4'>
+        <div className='text-black flex justify-start gap-4'>
           <input className='w-32'type='email' id='email' value={this.state.email} onChange={this.handleChange} />
           <span>|</span>
           <input className='w-28' type='tel' id='number' value={this.state.number} onChange={this.handleChange} />
