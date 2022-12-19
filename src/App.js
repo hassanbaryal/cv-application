d3dff42585bee181b5eeba2d401b27a5855d4654
hassanbaryal <h.baryal2@gmail.com>
2022-12-19 11:35:23 -0500

    Imported work experience component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import GeneralInformation from './components/GeneralInformation';
 import EducationalExperience from './components/EducationalExperience';
+import WorkExperience from './components/WorkExperience';
 
 class App extends React.Component {
 
@@ -13,6 +14,7 @@ class App extends React.Component {
       <div className="w-8/12 min-h-full p-7 bg-white shadow-md shadow-gray-800/50 flex flex-col gap-4">
         <GeneralInformation />
         <EducationalExperience />
+        <WorkExperience />
       </div>
     );
   };
